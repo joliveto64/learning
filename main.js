3208812cda9eb42dcfa704f00eec24005f9f127c
joliveto64 <joliveto64@gmail.com>
2023-10-23 10:37:42 +0100

    fixed linked list

--- main.js
+++ main.js
@@ -1018,3 +1018,82 @@ function hasCycle(head) {
   }
   return false;
 }
+
+// double linked list
+
+class Node3 {
+  constructor(data, prev = null, next = null) {
+    this.data = data;
+    this.prev = prev;
+    this.next = next;
+  }
+}
+
+class linkedList3 {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(data) {
+    let newNode = new Node3(data);
+
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+  }
+
+  prepend(data) {
+    let newNode = new Node3(data);
+
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
+  }
+
+  insertInside(data) {
+    let newNode = new Node(data);
+    let next = this.head.next;
+
+    this.head.next = newNode;
+    newNode.prev = this.head;
+    newNode.next = next;
+    next.prev = newNode;
+  }
+
+  deleteFirst() {
+    this.head = this.head.next;
+  }
+
+  deleteLast() {
+    this.tail = this.tail.prev;
+    this.tail.next = null;
+  }
+
+  getFirst() {
+    return this.head.data;
+  }
+
+  getLast() {
+    return this.tail.data;
+  }
+
+  getLength() {
+    let count = 1;
+    let current = this.head;
+
+    while (current.next) {
+      count++;
+      current = current.next;
+    }
+    return count;
+  }
+
+  printData() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
