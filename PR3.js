064f1e67f0f3dac773849864fb4b114233a8a881
joliveto64 <joliveto64@gmail.com>
2023-10-30 14:56:15 +0000

    added merge linked list problem

--- PR3.js
+++ PR3.js
@@ -130,3 +130,25 @@ class leastRecentlyUsedCache {
     if (node === this.tail) this.tail = node.prev;
   }
 }
+
+// leetcode merge two sorted lists (#21)
+
+function mergeTwoLists(list1, list2) {
+  let head = new ListNode();
+  let current = head;
+
+  while (list1 && list2) {
+    if (list1.val <= list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  current.next = list1 ? list1 : list2;
+
+  return head.next;
+}
