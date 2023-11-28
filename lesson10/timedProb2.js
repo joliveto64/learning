dd0341446359f4df59e6abf496e5661e7665cf47
joliveto64 <joliveto64@gmail.com>
2023-11-28 14:12:43 +0000

    finished timedProb2

--- lesson10/timedProb2.js
+++ lesson10/timedProb2.js
@@ -35,7 +35,7 @@ function trappingRainWater(height) {
 
     // unplanned addition to handle bug
     if (i === height.length - 1 && height[i] < wallHeight) {
-      wallHeight = 0;
+      wallHeight = height[i];
       tempTotal = 0;
       i = wallIndex;
     }
@@ -43,11 +43,89 @@ function trappingRainWater(height) {
   return totalWater;
 }
 
-console.log(trappingRainWater([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
-console.log(trappingRainWater([4, 2, 0, 3, 2, 5]));
+// console.log(trappingRainWater([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
+// console.log(trappingRainWater([4, 2, 0, 3, 2, 5]));
+// console.log(trappingRainWater([4, 2, 3]));
 
-// have a solution that works on both of the leet example cases right now, about 4min left. Took me a little bit to figure out how to add 34-38 to fix a bug. I had to account for the loop ending before it found another wall that was >= to the previous highest wall. This stops it from adding water it couldn't actually collect by resetting the index to +1 after previous wall and resetting the wall height and tempWater
+// have a solution that works on both of the leet example cases right now, about 4min left. Fails some other tests though. Failed [4,2,3], I see why. Lame.
 
-// time complexity is n + k where k is equal to the space between the current highest wall and the end of the array. Worst case scenario is highest wall at index 1, then looping until the end because it will, at the end, reset to index 2 and do it all over again. In other words, worst case should be n + (n - 1). Space is the input size n plus O(1) for the constant space variables
+function trappingRainWater2(height) {
+  // trim off the end if it's descending
+  let currentIndex = height.length - 1;
+  while (height[currentIndex] < height[currentIndex - 1]) {
+    height.pop();
+    currentIndex--;
+  }
 
-// I think there's probably a clever two-pointer solution which would be faster where you'd do the same thing but from boths sides at the same time, then handle what happens when the pointers intersect or pass each other
+  // then do same things
+  let wallHeight = 0;
+  let totalWater = 0;
+  let tempTotal = 0;
+  let wallIndex;
+
+  for (let i = 0; i < height.length; i++) {
+    if (height[i] >= wallHeight) {
+      wallHeight = height[i];
+      totalWater += tempTotal;
+      tempTotal = 0;
+      wallIndex = i;
+    }
+
+    if (height[i] < wallHeight) {
+      tempTotal += wallHeight - height[i];
+    }
+
+    if (i === height.length - 1 && height[i] < wallHeight) {
+      wallHeight = height[i];
+      tempTotal = 0;
+      i = wallIndex;
+    }
+  }
+  return totalWater;
+}
+
+// console.log(trappingRainWater2([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
+// console.log(trappingRainWater2([4, 2, 0, 3, 2, 5]));
+// console.log(trappingRainWater2([4, 2, 3]));
+// console.log(trappingRainWater2([4, 9, 4, 5, 3, 2]));
+// console.log(trappingRainWater2([9, 6, 8, 8, 5, 6, 3]));
+
+// okay have spent too long and asked chatGPT. It says to use 2 pointers. I thought of using two pointers, but with my previous solution just working in two ways at once. After running through it in my head I disregarded the idea
+
+function chatGptIsALittleBitch(height) {
+  let left = 0;
+  let maxLeft = 0;
+  let right = height.length - 1;
+  let maxRight = 0;
+  let trappedWater = 0;
+
+  while (left < right) {
+    if (height[left] < height[right]) {
+      if (height[left] >= maxLeft) {
+        maxLeft = height[left];
+      } else {
+        trappedWater += maxLeft - height[left];
+      }
+      left++;
+    } else {
+      if (height[right] >= maxRight) {
+        maxRight = height[right];
+      } else {
+        trappedWater += maxRight - height[right];
+      }
+      right--;
+    }
+  }
+  return trappedWater;
+}
+
+console.log("should be: 6");
+console.log(chatGptIsALittleBitch([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
+console.log("should be: 9");
+console.log(chatGptIsALittleBitch([4, 2, 0, 3, 2, 5]));
+console.log("should be: 1");
+console.log(chatGptIsALittleBitch([4, 2, 3]));
+console.log("should be: 1");
+console.log(chatGptIsALittleBitch([4, 9, 4, 5, 3, 2]));
+console.log("should be: 3");
+console.log(chatGptIsALittleBitch([9, 6, 8, 8, 5, 6, 3]));
