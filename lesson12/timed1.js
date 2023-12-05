b2936651add50f28dccf196f93952670fe597a0c
John <joliveto64@gmail.com>
2023-12-05 12:57:19 +0000

    added timed2

--- lesson12/timed1.js
+++ lesson12/timed1.js
@@ -34,10 +34,11 @@ function subsequence(sub, target) {
   return false;
 }
 
-console.log(subsequence("abc", "ahbgdc"));
-console.log(subsequence("axc", "ahbgdc"));
-console.log(subsequence("''", "''"));
-
 // at 46min left / 1hr right now, had to change something to pass 1 edge case on leet. Although I call BS because they said constraints were only lowercase english letter then the last test is "" and "" so fuck them leet can suck it.
 
 // 5. improve. I loop through the target once, so the time is n where n is the length of the target in the worst case. I don't want to move into a map or anything because I need to retain order. Also, I need to look at everything to know for sure. I didn't do a nested loop so that's fine. Space is input plus two variables which are O(1). I'm comfortable with this solution, not gonna change it. 43min left.
+
+// 7. test!
+// console.log(subsequence("abc", "ahbgdc"));
+// console.log(subsequence("axc", "ahbgdc"));
+// console.log(subsequence("''", "''"));
